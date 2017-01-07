ca86efd49d98ad1e4ff191419dd1836cb4aa6c54
charlietw <cwren92@gmail.com>
2017-01-07 19:32:54 +0000

    style: Amend project.py to conform to pep8 styleguide

--- catalog/project.py
+++ catalog/project.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, \
+  redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Supplier, Meal, User
 from flask import session as login_session
 import random
 import string
-
-# IMPORTS FOR THIS STEP
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
@@ -23,13 +20,12 @@ CLIENT_ID = json.loads(
 APPLICATION_NAME = "Restaurant Menu Application"
 
 
-#Connect to Database and create database session
+# Connect to Database and create database session
 engine = create_engine('sqlite:///mealscatalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
 
 
 # Create anti-forgery state token
@@ -38,8 +34,8 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.html', STATE = state)
-    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -93,7 +89,7 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
+        response = make_response(json.dumps('User is already connected.'),
                                  200)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -116,56 +112,52 @@ def gconnect():
     # See if user exists, if it doesn't, make a new one.
 
     try:
-      user_id = getUserID(login_session['email'])
-      if not user_id:
-        createUser(login_session)
-      login_session['user_id'] = user_id
-      print user_id
-      print login_session['user_id']
+        user_id = getUserID(login_session['email'])
+        if not user_id:
+            createUser(login_session)
+        login_session['user_id'] = user_id
+        print user_id
+        print login_session['user_id']
     except:
-      print "Error"
-
-
+        print "Error"
 
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
     output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("You are now logged in as %s." % login_session['username'])
     print "done!"
     return output
 
+
 def createUser(login_session):
-      newUser = User(name=login_session['username'], email=login_session[
-                     'email'], picture=login_session['picture'])
-      session.add(newUser)
-      session.commit()
-      user = session.query(User).filter_by(email=login_session['email']).one()
-      return user.id
+    newUser = User(name=login_session['username'], email=login_session[
+                    'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
 
 
 def getUserInfo(user_id):
-      user = session.query(User).filter_by(id=user_id).one()
-      return user
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
 
 
 def getUserID(email):
-      try:
-          user = session.query(User).filter_by(email=email).one()
-          return user.id
-      except:
-          return None
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
 
 
 @app.route('/gdisconnect')
 def gdisconnect():
     if 'username' not in login_session:
-    	flash("You were not logged in!")
-    	print "Not logged in."
-    	return redirect(url_for('home'))
+        flash("You were not logged in!")
+        print "Not logged in."
+        return redirect(url_for('home'))
     credentials = login_session['credentials']
     print credentials
     access_token = credentials
@@ -174,49 +166,55 @@ def gdisconnect():
     print 'User name is: '
     print login_session['username']
     if access_token is None:
-      print 'Access Token is None'
-      response = make_response(json.dumps('Current user not connected.'), 401)
-      response.headers['Content-Type'] = 'application/json'
-      return response
+        print 'Access Token is None'
+        response = make_response(
+                    json.dumps('Current user not connected.'), 401
+                    )
+        response.headers['Content-Type'] = 'application/json'
+        return response
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print 'result is '
     print result
     if result['status'] == '200':
-      del login_session['credentials']
-      del login_session['gplus_id']
-      del login_session['username']
-      del login_session['email']
-      del login_session['picture']
-      del login_session['user_id']
-      response = make_response(json.dumps('Successfully disconnected.'), 200)
-      response.headers['Content-Type'] = 'application/json'
-      print response
-      flash("Successfully logged out.")
-      return redirect(url_for('home'))
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        print response
+        flash("Successfully logged out.")
+        return redirect(url_for('home'))
     else:
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-      response = make_response(json.dumps('Failed to revoke token for given user.', 400))
-      response.headers['Content-Type'] = 'application/json'
-      return response
 
-#JSON APIs to view supplier Information
+# JSON APIs to view supplier Information
 @app.route('/supplier/<int:supplier_id>/meals/JSON')
 def supplierMenuJSON(supplier_id):
-    supplier = session.query(Supplier).filter_by(id = supplier_id).one()
-    items = session.query(Meal).filter_by(supplier_id = supplier_id).all()
+    supplier = session.query(Supplier).filter_by(id=supplier_id).one()
+    items = session.query(Meal).filter_by(supplier_id=supplier_id).all()
     return jsonify(Meals=[i.serialize for i in items])
+
 
 @app.route('/supplier/<int:supplier_id>/meals/<int:meal_id>/JSON')
 def mealJSON(supplier_id, meal_id):
-    meal = session.query(Meal).filter_by(id = meal_id).one()
+    meal = session.query(Meal).filter_by(id=meal_id).one()
     return jsonify(Meal=meal.serialize)
+
 
 @app.route('/meals/JSON')
 def mealsJSON():
     meals = session.query(Meal).all()
     return jsonify(Meals=[r.serialize for r in meals])
+
 
 @app.route('/suppliers/JSON')
 def suppliersJSON():
@@ -228,121 +226,121 @@ def suppliersJSON():
 @app.route('/')
 @app.route('/home/')
 def home():
-  suppliers = session.query(Supplier).order_by(asc(Supplier.name))
-  print login_session
-  # if 'username' not in login_session:
-  return render_template('home.html', suppliers = suppliers)
-  # return render_template('publicrestaurants.html', restaurants = restaurants)
+    suppliers = session.query(Supplier).order_by(asc(Supplier.name))
+    return render_template('home.html', suppliers=suppliers)
+
 
 @app.route('/suppliers')
 def suppliers():
-  suppliers = session.query(Supplier).order_by(asc(Supplier.name))
-  print login_session
-  # if 'username' not in login_session:
-  return render_template('suppliers.html', suppliers = suppliers)
-  # return render_template('publicrestaurants.html', restaurants = restaurants)
+    suppliers = session.query(Supplier).order_by(asc(Supplier.name))
+    print login_session
+    return render_template('suppliers.html', suppliers=suppliers)
 
 
-# #Create a new supplier
-@app.route('/supplier/new/', methods=['GET','POST'])
+# Create a new supplier
+@app.route('/supplier/new/', methods=['GET', 'POST'])
 def newSupplier():
-  if 'username' not in login_session:
-    return redirect ('/login')
-  if request.method == 'POST':
-      newSupplier = Supplier(name = request.form['name'], user_id=login_session['user_id'])
-      session.add(newSupplier)
-      flash('New Supplier %s successfully created' % newSupplier.name)
-      session.commit()
-      return redirect(url_for('home'))
-  else:
-      return render_template('newSupplier.html')
+    if 'username' not in login_session:
+        return redirect('/login')
+    if request.method == 'POST':
+        newSupplier = Supplier(name=request.form['name'],
+                               user_id=login_session['user_id'])
+        session.add(newSupplier)
+        flash('New Supplier %s successfully created' % newSupplier.name)
+        session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('newSupplier.html')
 
-#Edit a supplier
-@app.route('/supplier/<int:supplier_id>/edit/', methods = ['GET', 'POST'])
+
+# Edit a supplier
+@app.route('/supplier/<int:supplier_id>/edit/', methods=['GET', 'POST'])
 def editSupplier(supplier_id):
-	if 'username' not in login_session:
-		return redirect ('/login')
-	editedSupplier = session.query(Supplier).filter_by(id = supplier_id).one()
-	if request.method == 'POST':
-	  if request.form['name']:
-	    editedSupplier.name = request.form['name']
-	    session.commit()
-	    flash('Successfully renamed supplier to %s' % editedSupplier.name)
-	    return redirect(url_for('suppliers'))
-	return render_template('editSupplier.html', supplier = editedSupplier)
+    if 'username' not in login_session:
+        return redirect('/login')
+    editedSupplier = session.query(Supplier).filter_by(id=supplier_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedSupplier.name = request.form['name']
+            session.commit()
+            flash('Successfully renamed supplier to %s' % editedSupplier.name)
+            return redirect(url_for('suppliers'))
+    return render_template('editSupplier.html', supplier=editedSupplier)
 
 
-#Show a supplier menu
+# Show a supplier menu
 @app.route('/supplier/<int:supplier_id>/')
 @app.route('/supplier/<int:supplier_id>/menu/')
 def showMenu(supplier_id):
-    supplier = session.query(Supplier).filter_by(id = supplier_id).one()
-    meals = session.query(Meal).filter_by(supplier_id = supplier_id).all()
-    # supplierid = supplier.user_id
-    # if 'username' in login_session:
-    #   if supplierid == login_session['user_id']:
-    #     return render_template('menu.html', meals = meals, supplier = supplier)
-    return render_template('meals.html', meals = meals, supplier = supplier)
+    supplier = session.query(Supplier).filter_by(id=supplier_id).one()
+    meals = session.query(Meal).filter_by(supplier_id=supplier_id).all()
+    return render_template('meals.html', meals=meals, supplier=supplier)
 
 
-#Create a new meal
-@app.route('/newmeal/',methods=['GET','POST'])
+# Create a new meal
+@app.route('/newmeal/', methods=['GET', 'POST'])
 def newMeal():
-	if 'username' not in login_session:
-		return redirect ('/login')
-	suppliers = session.query(Supplier).all()
-	if request.method == 'POST':
-	    newMeal = Meal(name = request.form['name'], description = request.form['description'], price = request.form['price'], supplier_id = request.form['supplier'])
-	    session.add(newMeal)
-	    session.commit()
-	    flash('New meal "%s" successfully created' % (newMeal.name))
-	    return redirect(url_for('home'))
-	else:
-	    return render_template('newMeal.html', suppliers = suppliers)
+    if 'username' not in login_session:
+        return redirect('/login')
+    suppliers = session.query(Supplier).all()
+    if request.method == 'POST':
+        newMeal = Meal(name=request.form['name'],
+                       description=request.form['description'],
+                       price=request.form['price'],
+                       supplier_id=request.form['supplier'])
+        session.add(newMeal)
+        session.commit()
+        flash('New meal "%s" successfully created' % (newMeal.name))
+        return redirect(url_for('home'))
+    else:
+        return render_template('newMeal.html', suppliers=suppliers)
 
 
-#Edit a meal
-@app.route('/supplier/<int:supplier_id>/menu/<int:meal_id>/edit', methods=['GET','POST'])
+# Edit a meal
+@app.route('/supplier/<int:supplier_id>/menu/<int:meal_id>/edit',
+           methods=['GET', 'POST'])
 def editmeal(supplier_id, meal_id):
-	if 'username' not in login_session:
-		return redirect ('/login')
-	editedMeal = session.query(Meal).filter_by(id = meal_id).one()
-	supplier = session.query(Supplier).filter_by(id = supplier_id).one()
-	if request.method == 'POST':
-		if request.form['name']:
-			editedMeal.name = request.form['name']
-		if request.form['description']:
-			editedMeal.description = request.form['description']
-		if request.form['price']:
-			editedMeal.price = request.form['price']
+    if 'username' not in login_session:
+        return redirect('/login')
+    editedMeal = session.query(Meal).filter_by(id=meal_id).one()
+    supplier = session.query(Supplier).filter_by(id=supplier_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedMeal.name = request.form['name']
+        if request.form['description']:
+            editedMeal.description = request.form['description']
+        if request.form['price']:
+            editedMeal.price = request.form['price']
 
-		session.add(editedMeal)
-		session.commit()
-		flash('Menu Item Successfully Edited')
-		return redirect(url_for('showMenu', supplier_id = supplier_id))
-	else:
-		return render_template('editmeal.html', supplier_id = supplier_id, meal_id = meal_id, item = editedMeal)
-
-
-#Delete a menu item
-@app.route('/supplier/<int:supplier_id>/menu/<int:meal_id>/delete', methods = ['GET','POST'])
-def deleteMeal(supplier_id,meal_id):
-	if 'username' not in login_session:
-		return redirect ('/login')
-	supplier = session.query(Supplier).filter_by(id = supplier_id).one()
-	itemToDelete = session.query(Meal).filter_by(id = meal_id).one()
-	if request.method == 'POST':
-		session.delete(itemToDelete)
-		session.commit()
-		flash('Meal successfully deleted')
-		return redirect(url_for('showMenu', supplier_id = supplier_id))
-	else:
-		return render_template('deleteMeal.html', item = itemToDelete)
+        session.add(editedMeal)
+        session.commit()
+        flash('Menu Item Successfully Edited')
+        return redirect(url_for('showMenu', supplier_id=supplier_id))
+    else:
+        return render_template('editmeal.html',
+                               supplier_id=supplier_id,
+                               meal_id=meal_id,
+                               item=editedMeal)
 
 
+# Delete a menu item
+@app.route('/supplier/<int:supplier_id>/menu/<int:meal_id>/delete',
+           methods=['GET', 'POST'])
+def deleteMeal(supplier_id, meal_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    supplier = session.query(Supplier).filter_by(id=supplier_id).one()
+    itemToDelete = session.query(Meal).filter_by(id=meal_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash('Meal successfully deleted')
+        return redirect(url_for('showMenu', supplier_id=supplier_id))
+    else:
+        return render_template('deleteMeal.html', item=itemToDelete)
 
 
 if __name__ == '__main__':
-  app.secret_key = 'super_secret_key'
-  app.debug = True
-  app.run(host = '0.0.0.0', port = 5000)
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
